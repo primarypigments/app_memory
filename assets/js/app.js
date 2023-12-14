bae08f56079e8e8ed5958a543d03540bb61b1d57
primary pigments <primarypigments@gmail.com>
2023-12-14 20:27:08 +0000

    Add images to cards, total 16

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,66 @@ const memoryArray = [
     {
         name: 'aggression-1',
         img: 'assets/images/aggression-1.jpg',
-    }
+    },
 
-
+    {
+        name: 'aggression-2',
+        img: 'assets/images/aggression-2.jpg',
+    },
+    {
+        name: 'confidence-2',
+        img: 'assets/images/confidence-2.jpg',
+    },
+    {
+        name: 'confidence-3',
+        img: 'assets/images/confidence-3.jpg',
+    },
+    {
+        name: 'depression-2',
+        img: 'assets/images/depression-2.jpg',
+    },
+    {
+        name: 'depression-3',
+        img: 'assets/images/depression-3.jpg',
+    },
+    {
+        name: 'depression-4',
+        img: 'assets/images/depression-4.jpg',
+    },
+    {
+        name: 'found-img',
+        img: 'assets/images/found-img.jpg',
+    },
+    {
+        name: 'goals-img',
+        img: 'assets/images/goals-img.jpg',
+    },
+    {
+        name: 'meetings-img',
+        img: 'assets/images/meetings-img.jpg',
+    },
+    {
+        name: 'harmony-2',
+        img: 'assets/images/harmony-2.jpg',
+    },
+    {
+        name: 'harmony-2',
+        img: 'assets/images/harmony-2.jpg',
+    },
+    {
+        name: 'self-aware-1',
+        img: 'assets/images/self-aware-1.jpg',
+    },
+    {
+        name: 'self-aware-3',
+        img: 'assets/images/self-aware-3.jpg',
+    },
+    {
+        name: 'trauma-2',
+        img: 'assets/images/trauma-2.jpg',
+    },
+    {
+        name: 'trauma-3',
+        img: 'assets/images/trauma-3.jpg',
+    },
 ]
